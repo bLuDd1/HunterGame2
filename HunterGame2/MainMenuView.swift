eae79c330cbe8917378af85241515c1aedf6d9dd
bLuDd1 <dtavluj@gmail.com>
2024-08-14 22:44:36 +0300

    add logo and some layout design in menu

--- HunterGame2/MainMenuView.swift
+++ HunterGame2/MainMenuView.swift
@@ -13,8 +13,16 @@ struct MainMenuView: View {
             ZStack {
                 Image("game_background_3")
                 VStack {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 250)
+                        .offset(y: -200)
+                        
+                    
                     NavigationLink(destination: MapView()) {
                         Image("play1")
+                            .offset(y: -50)
                     }
                 }
             }
