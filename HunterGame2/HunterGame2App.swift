62c2a57be30ad5d620972950ed7536818b9e05a5
bLuDd1 <dtavluj@gmail.com>
2024-08-14 15:30:54 +0300

    Initial Commit

---
+++ HunterGame2/HunterGame2App.swift
@@ -0,0 +1,17 @@
+//
+//  HunterGame2App.swift
+//  HunterGame2
+//
+//  Created by Dima Tavlui on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct HunterGame2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
